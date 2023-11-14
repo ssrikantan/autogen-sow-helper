0f535eaa2071e77140b0a89583e9a05dd4916361
Srikantan Sankaran <s_srikantan@hotmail.com>
2023-11-14 07:45:50 +0000

    added conversation ability

--- bot-app.py
+++ bot-app.py
@@ -1,4 +1,13 @@
+import streamlit as st
+import asyncio
 import autogen
+
+from autogen.agentchat.contrib.retrieve_assistant_agent import RetrieveAssistantAgent
+from autogen.agentchat.contrib.retrieve_user_proxy_agent import RetrieveUserProxyAgent
+import chromadb
+
+
+st.title("autogen - SOW AI Assistant")
 
 config_list = autogen.config_list_from_json(
     env_or_file="OAI_CONFIG_LIST",
@@ -26,55 +35,73 @@ from autogen.retrieve_utils import TEXT_FORMATS
 print("Accepted file formats for `docs_path`:")
 print(TEXT_FORMATS)
 
-
-from autogen.agentchat.contrib.retrieve_assistant_agent import RetrieveAssistantAgent
-from autogen.agentchat.contrib.retrieve_user_proxy_agent import RetrieveUserProxyAgent
-import chromadb
-
-# 1. create an RetrieveAssistantAgent instance named "assistant"
-assistant = RetrieveAssistantAgent(
-    name="assistant", 
-    system_message="You are a helpful assistant.",
-    llm_config={
-        "timeout": 600,
-        "cache_seed": 42,
-        "config_list": config_list,
-    },
-)
-
-# 2. create the RetrieveUserProxyAgent instance named "ragproxyagent"
-# By default, the human_input_mode is "ALWAYS", which means the agent will ask for human input at every step. We set it to "NEVER" here.
-# `docs_path` is the path to the docs directory. It can also be the path to a single file, or the url to a single file. By default, 
-# it is set to None, which works only if the collection is already created.
-# 
-# Here we generated the documentations from FLAML's docstrings. Not needed if you just want to try this notebook but not to reproduce the
-# outputs. Clone the FLAML (https://github.com/microsoft/FLAML) repo and navigate to its website folder. Pip install and run `pydoc-markdown`
-# and it will generate folder `reference` under `website/docs`.
-#
-# `task` indicates the kind of task we're working on. In this example, it's a `code` task.
-# `chunk_token_size` is the chunk token size for the retrieve chat. By default, it is set to `max_tokens * 0.6`, here we set it to 2000.
-ragproxyagent = RetrieveUserProxyAgent(
-    name="ragproxyagent",
-    human_input_mode="NEVER",
-    max_consecutive_auto_reply=10,
-    retrieve_config={
-        "task": "code",
-        "docs_path": "/workspaces/autogen-sow-helper/all_docs/ites/sow",  # change this to your own path, such as https://raw.githubusercontent.com/microsoft/autogen/main/README.md
-        "chunk_token_size": 2000,
-        "model": config_list[1]["model"],
-        "client": chromadb.PersistentClient(path="/tmp/chromadb"),
-        "embedding_model": "all-mpnet-base-v2",
-        "get_or_create": True,  # set to True if you want to recreate the collection
-    },
-)
+class TrackableAssistantAgent(RetrieveAssistantAgent):
+    def _process_received_message(self, message, sender, silent):
+        with st.chat_message(sender.name):
+            st.markdown(message)
+        return super()._process_received_message(message, sender, silent)
 
 
-# reset the assistant. Always reset the assistant before starting a new conversation.
-assistant.reset()
+class TrackableUserProxyAgent(RetrieveUserProxyAgent):
+    def _process_received_message(self, message, sender, silent):
+        with st.chat_message(sender.name):
+            st.markdown(message)
+        return super()._process_received_message(message, sender, silent)
+    
 
-# given a problem, we use the ragproxyagent to generate a prompt to be sent to the assistant as the initial message.
-# the assistant receives the message and generates a response. The response will be sent back to the ragproxyagent for processing.
-# The conversation continues until the termination condition is met, in RetrieveChat, the termination condition when no human-in-loop is no code block detected.
-# With human-in-loop, the conversation will continue until the user says "exit".
-code_problem = "Of the SOW Documents for vendors Acme and Blue tech, for which one is the deposit amount in the payment terms the maximum?\n For the selected vendor, give me the total value of the contract\n Use the selected vendor's SOW as a reference and create the payment terms again, and apply an increase of 3% of the value for every year before the current date"
-ragproxyagent.initiate_chat(assistant, problem=code_problem, search_string="SOW")  # search_string is used as an extra filter for the embeddings search, in this case, we only want to search documents that contain "spark".
+with st.container():
+    # for message in st.session_state["messages"]:
+    #    st.markdown(message)
+
+    user_input = st.chat_input("Type in your query about creating SOW documents")
+    if user_input:
+        # create an AssistantAgent instance named "assistant"
+        # assistant = TrackableAssistantAgent(
+        #     name="assistant", llm_config=llm_config)
+        assistant = TrackableAssistantAgent(
+        name="assistant", 
+        system_message="You are a helpful assistant.",
+        llm_config={
+            "timeout": 600,
+            "cache_seed": 42,
+            "config_list": config_list,
+            },
+        )
+
+        # create a UserProxyAgent instance named "user"
+        # user_proxy = TrackableUserProxyAgent(
+        #     name="user", human_input_mode="NEVER", llm_config=llm_config)
+
+        user_proxy = TrackableUserProxyAgent(
+        name="ragproxyagent",
+        human_input_mode="NEVER",
+        max_consecutive_auto_reply=10,
+        retrieve_config={
+            "task": "code",
+            "docs_path": "/workspaces/autogen-sow-helper/all_docs/ites/sow",  # change this to your own path, such as https://raw.githubusercontent.com/microsoft/autogen/main/README.md
+            "chunk_token_size": 2000,
+            "model": config_list[1]["model"],
+            "client": chromadb.PersistentClient(path="/tmp/chromadb"),
+            "embedding_model": "all-mpnet-base-v2",
+            "get_or_create": True,  # set to True if you want to recreate the collection
+            },
+        )
+
+        # Create an event loop
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+        # Define an asynchronous function
+        # async def initiate_chat():
+        #     await user_proxy.a_initiate_chat(
+        #         assistant,
+        #         message=user_input,
+        #     )
+
+        async def initiate_chat():
+            try:
+                await user_proxy.initiate_chat(assistant, problem=user_input, search_string="SOW")
+            except:
+                pass
+        # Run the asynchronous function within the event loop
+        loop.run_until_complete(initiate_chat())
